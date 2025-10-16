1d5161bdee2ea91a011175e449b6464f86b436dc
aenlco <renee@aenl.co>
2025-10-16 15:16:52 -0400

    made edits to cartitem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,31 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+  let totalCost = 0;
+        if (section === "plant") {
+            plantItems.forEach((item) => {
+                totalCost += item.cost * item.quantity;
+            });
+        }
+    return totalCost;
+    };
 
   const handleContinueShopping = (e) => {
-   
+  alert('Functionality to be added for future reference')
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(addItem(index));
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(index));
   };
 
   // Calculate total cost based on quantity for an item
